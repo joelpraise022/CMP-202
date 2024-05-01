77f96e66f5ec49bdaac1a419390cf9cb7b688e51
joelpraise022 <159132107+joelpraise022@users.noreply.github.com>
2024-05-01 11:38:22 +0100

    Create Aquatic.java

---
+++ Aquatic.java
@@ -0,0 +1,15 @@
+package OOP;
+
+public class Aquatic extends Animal {
+    @Override
+    String move() {
+        // TODO Auto-generated method stub
+        return "moves by swimming";
+    }
+
+    public static void main(String[] args) {
+        Aquatic fish = new Aquatic();
+        fish.name = "Sea horse";
+        System.out.println("\n" + fish.name + " " + fish.move());
+    }
+}
