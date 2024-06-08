865ec5cdb10d0d6e0c11b8bea7a3094ad7ac6916
joelpraise022 <159132107+joelpraise022@users.noreply.github.com>
2024-06-08 10:47:41 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+public static void main(String[] args) {
+    Person person = new Person();
+    person.setName("Joel");
+    System.out.println(person.getName());
+
+    person.setPassword("3458790");
+    System.out.println(person.getPassword());
+
+    Physics physics = new Physics();
+    System.out.println(physics.initialVelocity1(76, 4);
+    System.out.println(physics.calculateVelocity(43, 53, 6));
+}
